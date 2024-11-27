ef1dc7cc25de372a716f22892242c0a3c2f823b1
luckyxd-biz <imthebeast@outlook.in>
2024-11-27 15:21:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Bot Configuration
-BOT_TOKEN = ''  # Replace with your actual bot token
+BOT_TOKEN = '7560925100:AAHPBCKPip4ef7pL8pN0rhXue2Mabdv3T4E'  # Replace with your actual bot token
 BOT_USERNAME = 'swiftservexbot'  # Replace with your bot's username
 
 # API Keys and Tokens
