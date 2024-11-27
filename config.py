3c67fd594dbc8e93cedf9a61a036f7ae131826bb
luckyxd-biz <imthebeast@outlook.in>
2024-11-27 15:21:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Bot Configuration
-BOT_TOKEN = '7920311542:AAGsX2Q60J-mFUm2Oj9o4KWJSNppUx3y_5M'  # Replace with your actual bot token
-BOT_USERNAME = 'YourBotUsername'  # Replace with your bot's username
+BOT_TOKEN = ''  # Replace with your actual bot token
+BOT_USERNAME = 'swiftservexbot'  # Replace with your bot's username
 
 # API Keys and Tokens
-GOOGLE_API_KEY = "AIzaSyCcH4a2Zq5Gvrn_ycb_1Py-Z-4VsU6vyL0"
-SEARCH_ENGINE_ID = "b341ba8ffe8ee4a14"
+GOOGLE_API_KEY = "AIzaSyADbwRHdEK-J5sqgtvaVv4Kz_H9ymax7KE"
+SEARCH_ENGINE_ID = "96750e7f8dfab47e1"
 IPINFO_TOKEN = "4dd04ebcd1b3c1"
 
 
